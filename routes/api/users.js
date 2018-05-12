6baf623a97f4a5e592e703657d8863bdbc450e56
ihab-alazzawi <ihab.alazzawi@outlook.com>
2018-05-11 21:51:27 -0500

    add signin to users route

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,11 +9,11 @@ const bcrypt = require('bcryptjs');
 //@Access Public
 router.get('/test', (req, res) => console.log(res.json({ msg: 'Users API' })));
 
-//@Route POST api/users/register
+//@Route POST api/users/signup
 //@Desc  Register user
 //@Access Public
 
-router.post('/register', (req, res) => {
+router.post('/signup', (req, res) => {
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res.status(400).json({ email: 'Email already exists' });
@@ -42,6 +42,34 @@ router.post('/register', (req, res) => {
       });
     }
   });
+});
+
+//@Route POST api/users/signin
+//@Desc  signin User / Returning JWT
+//@Access Public
+
+router.post('/signin', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  //Find user by email
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        res.status(404).json({ email: 'User not found' });
+      }
+      //Check password
+      bcrypt
+        .compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({ msg: 'Success' });
+          } else {
+            return res.status(404).json({ password: 'Password incorrect' });
+          }
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
