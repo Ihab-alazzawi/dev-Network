1bdf6d0d3eb707bcfdb97eb3e74b61eff39333c4
ihab-alazzawi <ihab.alazzawi@outlook.com>
2018-07-03 02:23:00 -0500

    add/remove comments to posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -165,4 +165,95 @@ router.post(
   }
 );
 
+// @route   POST api/posts/comment/:id
+// @desc    Add comment to post
+// @access  Private
+
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    //check Validation
+    if (!isValid) {
+      //if any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+        //Add to comments array
+        post.comments.unshift(newComment);
+
+        //Save to DB
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postNotFound: 'Post Not Found' }));
+  }
+);
+
+// @route   PUT api/posts/comment/:id
+// @desc    Edit comment
+// @access  Private
+
+// router.put(
+//   '/comment/:id/:comment_id',
+//   passport.authenticate('jwt', { session: false }),
+//   (req, res) => {
+//     const updatedComment = {
+//       text: req.body.text,
+//       name: req.body.name,
+//       avatar: req.body.avatar,
+//       user: req.user.id,
+//       updatedAt: Date.now()
+//     };
+
+//     Post.findOneAndUpdate({ _id: req.params.comment_id }, updatedComment, {
+//       new: true
+//     })
+//       .then(post => res.json(post))
+//       .catch(err => res.status(404).json({ postNotFound: 'Post Not Found' }));
+//   }
+// );
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Remove comment from post
+// @access  Private
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        // Check to see if comment exists
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentnotexists: 'Comment does not exist' });
+        }
+
+        // Get remove index
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        // Splice comment out of array
+        post.comments.splice(removeIndex, 1);
+
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+  }
+);
+
 module.exports = router;
