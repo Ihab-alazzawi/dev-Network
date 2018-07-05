a397d44b73935094ae1e617f5b9287de3eec8598
ihab-alazzawi <ihab.alazzawi@outlook.com>
2018-07-05 17:06:14 -0500

    add redux and setup redux store with devtool ext

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
 import Signup from './components/auth/Signup';
 import Signin from './components/auth/Signin';
-
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/signin" component={Signin} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/signin" component={Signin} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
