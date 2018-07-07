0ea54c60aa14a98b1d39d3a6c5a3d6eadf101cae
ihab-alazzawi <ihab.alazzawi@outlook.com>
2018-07-07 01:05:45 -0500

    redux signin action and set current user with login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
 import { Provider } from 'react-redux';
 import store from './store';
 import Navbar from './components/layout/Navbar';
@@ -8,6 +11,16 @@ import Footer from './components/layout/Footer';
 import Signup from './components/auth/Signup';
 import Signin from './components/auth/Signin';
 import './App.css';
+
+//check for token
+if (localStorage.jwtToken) {
+  //set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  //decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
