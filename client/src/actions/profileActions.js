30ab1a2121281a126044af1d5f7ae90f0619166b
ihab-alazzawi <ihab.alazzawi@outlook.com>
2018-07-16 23:48:03 -0500

    create Experience component and display it with delete on dashboard

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -82,7 +82,26 @@ export const addEducation = (eduData, history) => dispatch => {
     );
 };
 
-//delete account
+//delete experience
+
+export const deleteExperience = id => dispatch => {
+  axios
+    .delete(`/api/profile/experience/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//delete account and profile
 
 export const deleteAccount = () => dispatch => {
   if (window.confirm('Are you sure? THIS CAN NOT BE UNDONE!')) {
