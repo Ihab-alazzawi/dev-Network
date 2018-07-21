7226efd414b445a30100b5636b81db316ca15ae6
Ihab-AL <ihab.alazzawi@outlook.com>
2018-07-20 23:45:37 -0500

    add functionality to ProfileHeader component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -10,6 +10,7 @@ class Navbar extends Component {
     e.preventDefault();
     this.props.clearCurrentProfile();
     this.props.signOutUser();
+    this.props.history.push('/signin');
   }
 
   render() {
