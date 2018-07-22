53110c1086c6688a74019b674d908503a364943b
Ihab-AL <ihab.alazzawi@outlook.com>
2018-07-22 16:52:30 -0500

    create PostFeed and PostItem components

---
+++ client/src/components/posts/PostItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PostItem = () => {
+  return (
+    <div>
+      <h1>Post Item</h1>
+    </div>
+  );
+};
+
+PostItem.propTypes = {};
+
+export default PostItem;
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import PostForm from './PostForm';
+import PostFeed from './PostFeed';
+import Spinner from '../common/Spinner';
+import { getPosts } from '../../actions/postActions';
 
 class Posts extends Component {
+  componentDidMount() {
+    this.props.getPosts();
+  }
   render() {
+    const { posts, loading } = this.props.post;
+    let postContent;
+    if (posts === null || loading) {
+      postContent = <Spinner />;
+    } else {
+      postContent = <PostFeed posts={posts} />;
+    }
     return (
       <div className="feed">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
               <PostForm />
+              {postContent}
             </div>
           </div>
         </div>
@@ -19,12 +33,15 @@ class Posts extends Component {
   }
 }
 
-// Posts.propTypes = {
-//   post: PropTypes.object.isRequired,
-//   addPost: PropTypes.func.isRequired
-// };
+Posts.propTypes = {
+  post: PropTypes.object.isRequired,
+  getPosts: PropTypes.func.isRequired
+};
 
-// const mapStateToProps = state => ({
-//   post: state.post
-// });
-export default connect(null)(Posts);
+const mapStateToProps = state => ({
+  post: state.post
+});
+export default connect(
+  mapStateToProps,
+  { getPosts }
+)(Posts);
