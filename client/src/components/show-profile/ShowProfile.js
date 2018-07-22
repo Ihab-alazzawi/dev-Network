90582b017c3fb34b7a7ad035e26a3baa38d7785c
Ihab-AL <ihab.alazzawi@outlook.com>
2018-07-22 12:03:52 -0500

    add NotFound component with 404 route

--- client/src/components/show-profile/ShowProfile.js
+++ client/src/components/show-profile/ShowProfile.js
@@ -16,6 +16,12 @@ class ShowProfile extends Component {
     }
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.profile.profile === null && this.props.profile.loading) {
+      this.props.history.push('/404');
+    }
+  }
+
   render() {
     const { profile, loading } = this.props.profile;
     let profileContent;
